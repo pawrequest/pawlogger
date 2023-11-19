a49a0e41656cab96569742f98cb9b1ea4ce14f35
prosodyspeaks <prosodyspeaks@gmail.com>
2023-11-19 20:15:02 +0000

    selective import on_new

--- loggingdecorators/__init__.py
+++ loggingdecorators/__init__.py
@@ -1,2 +1,9 @@
+import sys
+
 from .decorators import on_init, on_call
-from .on_new_dec_312 import on_new
+if sys.version_info >= (3, 12):
+    from loggingdecorators.future.on_new_dec_312 import on_new
+else:
+    if sys.version_info < (3, 8):
+        print("Unsupported Python version")
+    from loggingdecorators.on_new_dec_38 import on_new
