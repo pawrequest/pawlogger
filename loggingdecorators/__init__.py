d5f00d3299e80f57a98c70390ce04c7bab445152
prosodyspeaks <prosodyspeaks@gmail.com>
2023-11-20 05:08:08 +0000

    no future

--- loggingdecorators/__init__.py
+++ loggingdecorators/__init__.py
@@ -1,10 +1,11 @@
 # ruff: noqa: F401
 import sys
 
-from .decorators import on_init, on_call
-if sys.version_info >= (3, 12):
-    from loggingdecorators.future.on_new_dec_312 import on_new
-else:
-    if sys.version_info < (3, 8):
-        print("Unsupported Python version")
-    from loggingdecorators.on_new_dec_38 import on_new
+from .decorators import on_init, on_call, on_new
+
+# if sys.version_info >= (3, 12):
+#     from loggingdecorators.future.on_new_dec_312 import on_new
+# else:
+#     if sys.version_info < (3, 8):
+#         print("Unsupported Python version")
+#     from .decorators import on_new
