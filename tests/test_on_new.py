98eb603257e66c5fbbd9ab1a625defb8c26470b2
prosodyspeaks <prosodyspeaks@gmail.com>
2023-11-20 02:03:22 +0000

    rplace strings with consts in tests

--- tests/test_on_new.py
+++ tests/test_on_new.py
@@ -5,23 +5,30 @@ import uuid
 import pytest
 
 from loggingdecorators import on_new
-from loggingdecorators.on_new_dec_38 import DFLT_LOGGER_STR
+from loggingdecorators.decorators import DFLT_LOGGER_STR
 
 ARG1 = "value 1 for test"
 KWARG2 = "keyword value 2 for test"
+DFLT_KWARG2 = "default value kwarg 2"
+DFLT_KWARG3 = "default value kwarg 3"
 
 
-class SpuriousClass:
-    def __new__(cls, arg1, kwarg2='default_value_2', kwarg3="default_value_3"):
+class DummyClass:
+    def __new__(cls, arg1, kwarg2=DFLT_KWARG2, kwarg3=DFLT_KWARG3):
         return super().__new__(cls)
 
 
+NEW_CLASS_MSG = f"new: {DummyClass.__name__}"
+
+
 @pytest.fixture
-def test_logger(caplog):
+def test_logger():
     logger_name = "test_logger_" + str(uuid.uuid4())
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
     yield logger
+    logger.handlers.clear()
+    logger = None
 
 
 def apply_decorator(cls, **decorator_kwargs):
@@ -30,62 +37,67 @@ def apply_decorator(cls, **decorator_kwargs):
 
 
 def test_default_dec(caplog):
-    decorated_test_class = apply_decorator(SpuriousClass)
+    decorated_test_class = apply_decorator(DummyClass)
     with caplog.at_level(logging.DEBUG, logger=DFLT_LOGGER_STR):
         instance = decorated_test_class(ARG1, kwarg2=KWARG2)  # noqa: F841
     msg = caplog.records[0].msg
 
-    assert "new: SpuriousClass" in msg
+    assert NEW_CLASS_MSG in msg
     assert ARG1 in msg
-    assert f'kwarg2={KWARG2}' in msg
-    assert "default_value" not in msg
+    assert KWARG2 in msg
+    assert DFLT_KWARG2 not in msg
+    assert DFLT_KWARG3 not in msg
     caplog.clear()
 
 
 def test_with_logger_object(caplog, test_logger):
-    decorated_test_class = apply_decorator(SpuriousClass, logger=test_logger)
+    decorated_test_class = apply_decorator(DummyClass, logger=test_logger)
     with caplog.at_level(logging.DEBUG, logger=test_logger.name):
         instance = decorated_test_class(ARG1, kwarg2=KWARG2)  # noqa: F841
-    assert "new: SpuriousClass" in caplog.records[0].msg
-
-
-def test_no_logargs(caplog, test_logger):
-    caplog.clear()
-    decorated_test_class = apply_decorator(SpuriousClass, logger=test_logger.name, logargs=False)
-    instance = decorated_test_class(ARG1, kwarg2=KWARG2)  # noqa: F841
     msg = caplog.records[0].msg
-    assert "new: SpuriousClass" in msg
-    assert ARG1 not in msg
-    assert KWARG2 not in msg
-    assert "default_value" not in msg
-
-
-def test_log_defaults(caplog, test_logger):
-    caplog.clear()
-
-    decorated_test_class = apply_decorator(SpuriousClass, logger=test_logger, logargs=True, logdefaults=True)
-    with caplog.at_level(logging.DEBUG, logger=test_logger.name):
-        instance = decorated_test_class(ARG1)  # noqa: F841
-
-    assert "new: SpuriousClass" in caplog.text
-    assert ARG1 in caplog.text
-    assert "default_value_2" in caplog.text
-    assert "default_value_3" in caplog.text
+    assert NEW_CLASS_MSG in msg
 
 
 def test_with_callable(caplog, test_logger):
     def logger_callable():
         return test_logger
 
-    decorated_test_class = apply_decorator(SpuriousClass, logger=logger_callable)
+    decorated_test_class = apply_decorator(DummyClass, logger=logger_callable)
+    with caplog.at_level(logging.DEBUG, logger=test_logger.name):
+        instance = decorated_test_class(ARG1)  # noqa: F841
+    msg = caplog.records[0].msg
+    assert NEW_CLASS_MSG in msg
+
+
+def test_no_logargs(caplog, test_logger):
+    caplog.clear()
+    decorated_test_class = apply_decorator(DummyClass, logger=test_logger.name, logargs=False)
+    with caplog.at_level(logging.DEBUG, logger=test_logger.name):
+        instance = decorated_test_class(ARG1, kwarg2=KWARG2)  # noqa: F841
+    msg = caplog.records[0].msg
+    assert NEW_CLASS_MSG in msg
+    assert ARG1 not in msg
+    assert KWARG2 not in msg
+    assert DFLT_KWARG2 not in msg
+    assert DFLT_KWARG3 not in msg
+
+
+def test_log_defaults(caplog, test_logger):
+    caplog.clear()
+
+    decorated_test_class = apply_decorator(DummyClass, logger=test_logger, logargs=True, logdefaults=True)
     with caplog.at_level(logging.DEBUG, logger=test_logger.name):
         instance = decorated_test_class(ARG1)  # noqa: F841
 
-    assert "new: SpuriousClass" in caplog.text
+    msg = caplog.records[0].msg
+    assert NEW_CLASS_MSG in msg
+    assert ARG1 in msg
+    assert DFLT_KWARG2 in msg
+    assert DFLT_KWARG3 in msg
 
 
 def test_invalid_logger(caplog, test_logger):
     with pytest.raises(TypeError):
-        decorated_test_class = apply_decorator(SpuriousClass, logger=123)
+        decorated_test_class = apply_decorator(DummyClass, logger=123)
         with caplog.at_level(logging.DEBUG, logger=test_logger.name):
             instance = decorated_test_class(ARG1)  # noqa: F841
